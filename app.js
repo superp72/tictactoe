c43dadf7ff6e6310516ac65936feab02c4ea835c
Paul Peterson <superp72pp@gmail.com>
2016-06-29 06:23:23 +1000

    removed/disabled clicking on cell yippeegit status

--- app.js
+++ app.js
@@ -1,16 +1,97 @@
+
 var player = 1;
 function clickPos(pos){
-	if(player ===1){
+
+// if (content.innerHTML = "X" || content.innerHTML "O"){
+// 			return;
+
+// }
+if (player ===1){
+
+	
 	document.getElementById(pos).innerHTML = "X";
- 	document.getElementById(pos).disabled = "disabled";
+document.getElementById(pos).removeAttribute('onClick');
+	
  	console.log((pos) + " X");
+
 player -= 1;
-}else{
+
+
+}	
+
+else {
 	document.getElementById(pos).innerHTML = "O";
-	document.getElementById(pos).disabled = "disabled";
+ 
+ 	document.getElementById(pos).removeAttribute('onClick');
+
  	console.log((pos) + " O");
+
 player += 1;
 }
 }
 
+
+// var player = 1;
+// function clickPos(pos){
+// 	if(player ===1){
+// 	document.getElementById(pos).innerText = "X";
+// 	document.getElementById(pos).disabled = true;  
+
+//  	console.log((pos) + " X");
+// player -= 1;
+
+// else{
+// 	document.getElementById(pos).innerHTML = "O";
+// 	document.getElementById(pos).disabled = "disabled";
+//  	console.log((pos) + " O");
+// player += 1;
+// }
+// }
+// if(player ===1){
+// 	document.getElementById(pos).innerHTML = "X";
+// 	// return (pos) = 
+
+// }
+// // var player = 1;
+// function clickPos(pos){
+// 	if(player ===1){
+// 	document.getElementById(pos).innerHTML = "X";
+// 	else{
+// 		document.getElementById(pos).setAttribute("disabled", true);
+// 	}
+
+
+// document.getElementById(pos).disabled = "disabled";
+
 //Disabled not working
+// var player = 1;
+// function clickPos(pos){
+// 	if(player ===1){
+// 	document.getElementById(pos).innerHTML = "X";
+
+
+// 	}
+//  	// document.getElementById(pos).disabled = "disabled";
+//  	console.log((pos) + " X");
+// player -= 1;
+// }else{
+// 	document.getElementById(pos).innerHTML = "O";
+// 	document.getElementById(pos).disabled = "disabled";
+//  	console.log((pos) + " O");
+// player += 1;
+// }
+// }
+// var player = 1;
+// function clickPos(pos){
+
+// 	if(player ===1){
+// 	document.getElementById(pos).innerHTML = "X";
+//  	document.getElementById(pos).disabled = "disabled";
+// player -= 1;
+// }	
+// 	else{
+// 	document.getElementById(pos).innerHTML = "O";
+//  	document.getElementById(pos).disabled = "disabled";
+// player += 1;
+// }
+// }
