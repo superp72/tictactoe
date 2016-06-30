62dd72af4c40846728771a22247ccdd960b56178
Paul Peterson <superp72pp@gmail.com>
2016-07-01 00:39:43 +1000

    winners for X's & O's announced

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ function clickPos(pos){
 if (player ===1){
 	document.getElementById(pos).innerHTML = "X";
 	document.getElementById(pos).removeAttribute('onClick');
-	
- 	//console.log((pos) + " X");
-
+	 	//console.log((pos) + " X");
 	player -= 1;
 
 	}else {
@@ -19,56 +17,51 @@ if (player ===1){
 
 	player += 1;
 	}
-
-
-
  checkForWinner()
-
 }
 
-
 function checkForWinner(){
-	// an array for X tiles
-	var xArray = [];
-	// an array for O tiles
-	var oArray = [];
+	
+	var xArray = []; // an array for X tiles
+	var oArray = [];// an array for O tiles
 	var allTiles = document.getElementsByClassName("content");
 	console.log(allTiles);
-		for (var i = 0; i < 9; i++) {
 
-			// if allTiles[i].innerHTML === 'X'
-			if (allTiles[i].innerHTML === "X")
-			// 	push the allTiles[i].id.slice(3,4) into X array
-				xArray.push(allTiles[i].id.slice(3,4))
-			// else if innerhtml = 'O'
-			else if (allTiles[i].innerHTML === "O")
-			// 	push the allTiles[i].id.slice(3,4) into O array
-				oArray.push(allTiles[i].id.slice(3,4))
-		
-	}
-checkXandO(xArray, oArray);
-// 	//send our two arrays to a function that checks the arrays vs the master win array
+		for (var i = 0; i < 9; i++) {
+			if (allTiles[i].innerHTML === "X") // if allTiles[i].innerHTML === 'X'
+				xArray.push(allTiles[i].id.slice(3,4))// 	push the allTiles[i].id.slice(3,4) into X array
+			else if (allTiles[i].innerHTML === "O")// else if innerhtml = 'O'
+				oArray.push(allTiles[i].id.slice(3,4))// 	push the allTiles[i].id.slice(3,4) into O array
+		}
+checkXandO(xArray, oArray);//send our two arrays to a function that checks the arrays vs the master win array
 
 }
 
 function checkXandO (xArray, oArray){
 console.log(xArray, oArray);
 		var winner = false;
-
 		var win = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
 
-// // //for loop over the win array, grab each index ie: index 0 : 1,2,3
-			for (var i = 0; i < win.length; i++) {
+				for (var i = 0; i < win.length; i++) {//for loop over the win array, grab each index ie: index 0 : 1,2,3
 				var winArr = win[i];
-	  			if ( xArray.indexOf(winArr[0].toString()) != -1 && xArray.indexOf(winArr[1].toString()) != -1 && xArray.indexOf(winArr[2].toString()) != -1 ) {
-				winner = true; 
-				alert('winner X found');
-			/*	else if (oArray.indexOf(winArr[0].toString()) != -1 && oArray.indexOf(winArr[1].toString()) != -1 && oArray.indexOf(winArr[2].toString()) != -1)
-					winner = true; 
-				console.log('winner O found'); */
-		} 
+		  			if ( xArray.indexOf(winArr[0].toString()) != -1 && xArray.indexOf(winArr[1].toString()) != -1 && xArray.indexOf(winArr[2].toString()) != -1 ) {
+					// console.log(xArray.indexOf(winArr[0].toString());
+					// win[i] == xArray;
+					winner = true;
+					alert('winner X found');
+
+					}else if ( oArray.indexOf(winArr[0].toString()) != -1 && oArray.indexOf(winArr[1].toString()) != -1 && oArray.indexOf(winArr[2].toString()) != -1){
+					winner = true;
+					alert('winner O found');
+		// // 			winner = true;alert('winner O found'); 
+		// // 			else if (xArray.length + oArray.length == 9 && !winner) 
+		// // 			alert('draw');
+					// }
+		// // // 	}
+		// } 
 	}
 
+}
 }
 
 // //check X array to see if it contains 1,2,3
